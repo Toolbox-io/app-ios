8f13be0994c58e8641b622d209e1b5fa02200684
denis0001-dev <denis0001.dev@ya.ru>
2025-05-21 14:47:35 +0300

    Initial Commit

---
+++ Toolbox.io/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Toolbox.io
+//
+//  Created by denis0001-dev on 21.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Toolbox.io/Toolbox_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toolbox_ioApp.swift
+//  Toolbox.io
+//
+//  Created by denis0001-dev on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Toolbox_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
